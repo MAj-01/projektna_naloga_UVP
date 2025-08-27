ed5dcac18073dd071b10b7afc9fc1e646774e755
Maj Soklič <ms88068@student.uni-lj.si>
2025-08-27 15:45:03 +0200

    uredil kodo

--- zbiranje_podatkov/najstarejsi_prebivalci_po_drzavah.py
+++ zbiranje_podatkov/najstarejsi_prebivalci_po_drzavah.py
@@ -5,99 +5,70 @@ import re
 
 def podatki_o_najstarejsih_prebivalcih_po_drzavah():
     url = 'https://en.wikipedia.org/wiki/List_of_the_oldest_people_by_country'
-    prevzem = requests.get(url)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+        'AppleWebKit/537.36 (KHTML, like Gecko)'
+        'Chrome/91.0.4472.124 Safari/537.36'
+    }
+    prevzem = requests.get(url, headers=headers, timeout=10)
     page_content = prevzem.text
 
     abs_path = os.path.dirname(os.path.abspath(__file__))
     folder_path = os.path.join(abs_path, "..", "podatki")
     os.makedirs(folder_path, exist_ok=True)
 
+    # Shranimo podatke v html datoteko.
     html_path = os.path.join(folder_path, "najstarejsi_prebivalci_po_drzavah.html")
     with open(html_path, 'w', encoding='utf-8') as f:
         f.write(page_content)
 
-    # Sedaj moramo iz kode izlusciti le prvo tabelo na tej spletni strani. To bomo naredili z regularnimi izrazi.   
- 
-    vzorec = r'<h2 id="Oldest_ever">Oldest ever</h2>([\s\S]*?)<h3 id="Territorial_and_overseas_regions_recordholders">Territorial and overseas regions recordholders</h3>'
-    zozani_html_path = os.path.join(folder_path, "nastarejsi_prebivalci_po_drzavah_zozani_obseg.html")
-    match = re.search(vzorec, page_content, re.DOTALL | re.IGNORECASE)
-
-    if not match:
-        raise ValueError("Vzorca ni bilo mogoče najti")
-    
-    novi_page_content = match.group(1)
-
-
-    vzorec_tabele = r'<table class="wikitable sortable">([\s\S]*?)</table>'
-    match_tabele = re.search(vzorec_tabele, novi_page_content, re.DOTALL)
+    # Sedaj moramo iz kode izlusciti le tabelo na tej spletni strani.
+    # To bomo naredili z regularnim izrazom.    
+    vzorec_tabele = r'<table class([\s\S]*?)</table>'
+    match_tabele = re.findall(vzorec_tabele, page_content, re.DOTALL)
 
     if not match_tabele:
         raise ValueError("Tabele ni bilo mogoče najti")
     
-    tabela_content = match_tabele.group(1)
-
-    tabela_path = os.path.join(folder_path, "nastarejsi_prebivalci_po_drzavah_tabela.html")
-    with open(tabela_path, 'w', encoding='utf-8') as f:
-        f.write(tabela_content)
-
+    tabela_content = match_tabele[0]
+    
     # Sestavimo novi vzorec, ki bo zajel iz vsake vrstice določene podatke.
-
     seznam_slovarjev = []
     vrstice = re.findall(r'<tr.*?</tr>', tabela_content, flags=re.DOTALL)[1:]
-    
     prejsnja_drzava = None
     for vrstica in vrstice:
-        # Drzavo iz vrstice lahko pridobimo le iz linka slike, ki predstavlja zastavo drzave.
-        drzava = re.search(r'</span><a href="/wiki/([\s\S]*?)" title="([\s\S]*?)">([\s\S]*?)</a></td>', vrstica) 
-        if "Flag_of_" in vrstica and re.findall(r'</span><a href="/wiki/([\s\S]*?)" title="([\s\S]*?)">([\s\S]*?)</a></td>', vrstica) == []:
-            drzava = re.search(r'<a href="/wiki/([\s\S]*?)" title="([\s\S]*?)"><img alt="([\s\S]*?)"', vrstica)
-        if drzava:
-            drzava = drzava.group(2)  
-        else:
+        celice = re.findall(r'<td.*?>(.*?)</td>', vrstica, flags=re.DOTALL)
+
+        # Definiramo funkcijo za čiščenje podatkov, ki oznake HTML zamenja s praznimi nizi.
+        def ocisti_podatek(podatek):
+            return re.sub(r'<.*?>', '', podatek).strip()
+        
+        # Državo iz vrstice lahko pridobimo le iz linka slike, ki predstavlja zastavo države.
+        # Podatek o državi pa ni v vsaki vrstici (podatki o najstarejših prebivalcih so
+        # v podkategoriji držav, zato je podatek o državi le pri prvi osebi), zato moramo
+        # izbrati prejšnjo državo v vrsticah, ki ne vsebujejo podatka o državi.
+        if "Flag_of_" in celice[0]:
+            drzava = ocisti_podatek(celice[0])
+        else:   
             drzava = prejsnja_drzava
         prejsnja_drzava = drzava 
 
-        # Spol osebe je v vrsti zapisan v razdelku center, ki je v html kodi zapisan kot <div class="center">
-        spol = re.search(r'<div class="center">(.*?)</div></td>', vrstica)
-
-        # Datum rojstva in datum smrti osebe je v html kodi strani zapisan zaporedoma pod istimi oznakami, 
-        # zato uporabimo re.findall, da poiscemo oba zadetka
-        st_znacka = re.findall(r'<td([\s\S]*?)/td>', vrstica)
-        datum_smrti = None
-        datum_rojstva = None
-        if len(st_znacka) >= 4:
-            if "nowrap" in st_znacka[-3]:  
-                match_rojstvo = re.search(r'class="nowrap">(.*?)<', st_znacka[-3])  
-                datum_rojstva = match_rojstvo.group(1)
-            else:
-                # Če ni posebnih oznak, uporabimo celotno vsebino celice brez znakov < in >.
-                match_rojstvo = re.search(r'>(.*?)<', st_znacka[-3]) 
-                datum_rojstva = match_smrt.group(1)
-                   
-
-        if len(st_znacka) >= 4:
-            if "nowrap" in st_znacka[-2]:  
-                match_smrt = re.search(r'class="nowrap">(.*?)<', st_znacka[-2])  
-                datum_smrti = match_smrt.group(1)            
-            elif "Living" in st_znacka[-2]:
-                datum_smrti = "Living"
-            elif "reference" in st_znacka[-2]:
-                match_smrt = re.search(r'>(.*?)<sup', st_znacka[-2])
-                if match_smrt:
-                    datum_smrti = match_smrt.group(1)
-            else:
-                # Če ni posebnih oznak, uporabimo celotno vsebino celice brez znakov < in >.
-                match_smrt = re.search(r'>(.*?)<', st_znacka[-2]) 
-                datum_smrti = match_smrt.group(1)
-
-        # Določimo še starost osebe v letih in dnevih. To poiščemo v zadnjem sklopu <td> v vrstici.
-        leta = None
-        dnevi = None
-        match_datum = re.search(r'</span>(.*?)&#160;years, (.*?)&#160;days', st_znacka[-1])
+        # Spol osebe, datum rojstva in datum smrti so v vrstici zapisani vsak v svoji celici.
+        # Vse tri lahko pridobimo na isti način
+        spol = ocisti_podatek(celice[-4])
+        datum_rojstva = ocisti_podatek(celice[-3])
+        datum_smrti = ocisti_podatek(celice[-2])
+        datum_smrti= re.sub(r'&#\d+;', '', datum_smrti)  # Odstranimo morebitne HTML oznake.
+ 
+        # Določimo še starost osebe v letih in dnevih. Podatka najdemo v zadnji celici.       
+        match_datum = re.search(r'</span>(.*?)&#160;years, (.*?)&#160;days', celice[-1])
 
         seznam_slovarjev.append({
-                'Država': drzava,
-                'Spol': spol.group(1) if spol else None,
+                'Država': (
+                    drzava if '&#160;' not in drzava
+                    else drzava.replace('&#160;', '')
+                ),
+                'Spol': spol,
                 'Datum rojstva': datum_rojstva,
                 'Datum smrti': datum_smrti,
                 'Starost (leta)': match_datum.group(1),
@@ -105,12 +76,10 @@ def podatki_o_najstarejsih_prebivalcih_po_drzavah():
             })
 
     # Funkcija nazadnje podatke iz seznama slovarjev pretvori v csv datoteko.
-
     csv_path = os.path.join(folder_path, "najstarejsi_prebivalci_po_drzavah.csv")     
 
     if seznam_slovarjev:
         fieldnames = list(seznam_slovarjev[0].keys())
-        
         with open(csv_path, 'w', encoding='utf-8', newline='') as f:
             writer = csv.DictWriter(f, fieldnames=fieldnames)
             writer.writeheader()
--- zbiranje_podatkov/obnovljiva_energija_po_drzavah.py
+++ zbiranje_podatkov/obnovljiva_energija_po_drzavah.py
@@ -5,7 +5,13 @@ import re
 
 def podatki_o_obnovljivi_energiji_po_drzavah():
     url = 'https://en.wikipedia.org/wiki/List_of_countries_by_renewable_electricity_production'
-    prevzem = requests.get(url)
+    
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+        'AppleWebKit/537.36 (KHTML, like Gecko)'
+        'Chrome/91.0.4472.124 Safari/537.36'
+    }
+    prevzem = requests.get(url, headers=headers, timeout=10)
     page_content = prevzem.text
 
     abs_path = os.path.dirname(os.path.abspath(__file__))
@@ -16,25 +22,25 @@ def podatki_o_obnovljivi_energiji_po_drzavah():
     with open(html_path, 'w', encoding='utf-8') as f:
         f.write(page_content)
 
-    # Sedaj moramo iz kode izlusciti le prvo tabelo na tej spletni strani. To bomo naredili z regularnimi izrazi.   
- 
+    # Sedaj moramo iz kode izlusciti le prvo tabelo na tej spletni strani.
+    # To bomo naredili z regularnimi izrazi.   
     vzorec = r'<table([\s\S]*?)</table>'
     match = re.findall(vzorec, page_content, re.DOTALL | re.IGNORECASE)
 
     if not match:
         raise ValueError("Vzorca ni bilo mogoče najti")
     
+    # Izberemo pravo tabelo.
     tabela_content = match[2]
-
     tabela_path = os.path.join(folder_path, "nastarejsi_prebivalci_po_drzavah_tabela.html")
     with open(tabela_path, 'w', encoding='utf-8') as f:
         f.write(tabela_content)
 
     # Sestavimo novi vzorec, ki bo zajel iz vsake vrstice določene podatke.
-
     seznam_slovarjev = []
     vrstice = re.findall(r'<tr.*?</tr>', tabela_content, flags=re.DOTALL)[1:]
     
+    # Ustvarimo funkcijo za čiščenje podatkov, ki oznake HTML zamenja s praznimi nizi.
     def ocisti_podatek(podatek):
         return re.sub(r'<.*?>', '', podatek).strip()
 
@@ -42,20 +48,19 @@ def podatki_o_obnovljivi_energiji_po_drzavah():
         celice = re.findall(r'<td.*?>(.*?)</td>', vrstica, flags=re.DOTALL)
         # Drzavo iz vrstice lahko pridobimo le iz linka slike, ki predstavlja zastavo drzave.
         drzava = ocisti_podatek(celice[0])
+        drzava = re.sub(r'&#160;', '', drzava)
         procent_pridobljene_obnovljive_energije = ocisti_podatek(celice[1])
 
         seznam_slovarjev.append({
-                'Država': drzava[6:],
+                'Država': drzava,
                 'Odstotek pridobljene obnovljive energije': procent_pridobljene_obnovljive_energije
             })
 
     # Funkcija nazadnje podatke iz seznama slovarjev pretvori v csv datoteko.
-
     csv_path = os.path.join(folder_path, "obnovljiva_energija_po_drzavah.csv")     
 
     if seznam_slovarjev:
         fieldnames = list(seznam_slovarjev[0].keys())
-        
         with open(csv_path, 'w', encoding='utf-8', newline='') as f:
             writer = csv.DictWriter(f, fieldnames=fieldnames)
             writer.writeheader()
